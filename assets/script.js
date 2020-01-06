894c3929804fb034a83c594e60988ebb67c554f9
McCabe Northup <mccabenorthup@gmail.com>
2020-01-06 16:55:04 -0700

    added carousel, with some css. Currently app is not functional on mobile.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
+// selector and range in formDiv
 $(document).ready(function(){
     $('select').formSelect();
+  });
+// carousel functions
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
   });
 
 // AJAX call from 
 
-var appKEY = "14ea6a2a8ec5df04798b53f1975f47fb";
-var apiID = "0a8d88d0";
-queryURL = `https://api.edamam.com/search?q=chicken&app_id=${apiID}&app_key=${appKEY}&from=0&to=100&calories=591-722`	
-console.log(queryURL);
+// var appKEY = "14ea6a2a8ec5df04798b53f1975f47fb";
+// var apiID = "0a8d88d0";
+// queryURL = `https://api.edamam.com/search?q=chicken&app_id=${apiID}&app_key=${appKEY}&from=0&to=100&calories=591-722`	
+// console.log(queryURL);
 
 
-$.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response) {
-        console.log(response)
-    })
+// $.ajax({
+//         url: queryURL,
+//         method: 'GET'
+//     }).then(function(response) {
+//         console.log(response)
+//     })
