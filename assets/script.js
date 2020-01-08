278f16a1b7ad9aba32a9759ac10585c54db9d305
McCabe Northup <mccabenorthup@gmail.com>
2020-01-08 15:26:31 -0700

    filtered through multiple search results carousel will append new array

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.tabs').tabs();
-  });
+});
 
 
 
@@ -15,52 +15,38 @@ $('.recipeInputs').on('submit', function (e) {
     e.preventDefault();
     // variables for selectors
     var protein = $('#proteinInput').val();
-    var meal = $('#mealInput').val();
-    var diet = $('#dietInput').val();
     var serving = $('#servingInput').val();
     var cooking = $('#cookingInput').val();
     var range = $('#test5').val();
-    if (protein === 'Beef' || protein === 'Cheese' || protein === 'Chicken' || protein === 'Eggs' || protein === 'Fish' || protein === 'Fruit' || protein === 'Oats' || protein === 'Quinoa' || protein === 'Turkey' || protein === 'Pork' || protein === 'Vegetable' || protein === 'Yogurt' || protein === 'Apple' || protein === 'Lentils') {
-        // console.log(protein)
-        // console.log(meal)
-        // console.log(diet)
-        // console.log(serving)
-        console.log(cooking)
-        // console.log(range)
-        // AJAX call from 
-        var appKEY = "14ea6a2a8ec5df04798b53f1975f47fb";
-        var apiID = "0a8d88d0";
-        queryURL = `https://api.edamam.com/search?q=${protein}&app_id=${apiID}&app_key=${appKEY}&from=0&to=100&time=1%2B`
-        console.log(queryURL);
-        // &healthLabels=${}
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function (response) {
-            console.log(response);
-            var servingSize = response.hits.filter(function(size){
-                return size.recipe.yield == serving
-            })
-            var cookingTime = servingSize.filter(time => (time.recipe.totalTime <= cooking))
-            console.log(servingSize);
-            console.log(cookingTime);
-            
-            $('.foodImg1').attr('src', response.hits[0].recipe.image)
-            $('.a').html(response.hits[0].recipe.label)
-            $('.foodImg2').attr('src', response.hits[1].recipe.image)
-            $('.b').html(response.hits[1].recipe.label)
-            $('.foodImg3').attr('src', response.hits[2].recipe.image)
-            $('.c').html(response.hits[2].recipe.label)
-            $('.foodImg4').attr('src', response.hits[3].recipe.image)
-            $('.d').html(response.hits[3].recipe.label)
-            $('.foodImg5').attr('src', response.hits[4].recipe.image)
-            $('.e').html(response.hits[4].recipe.label)
-            $('.carousel').carousel();
-        });
-    } else {
-        $('.modal').modal();
-    }
 
+    // AJAX call from 
+    var appKEY = "14ea6a2a8ec5df04798b53f1975f47fb";
+    var apiID = "0a8d88d0";
+    queryURL = `https://api.edamam.com/search?q=${protein}&app_id=${apiID}&app_key=${appKEY}&from=0&to=100&time=1%2B`
+    // console.log(query
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        var filteredRecipes = response.hits;
+        if (serving) filteredRecipes = filteredRecipes.filter(item => item.recipe.yield == serving)
+        if (cooking) filteredRecipes = filteredRecipes.filter(item => item.recipe.totalTime <= cooking)
+        if (range) filteredRecipes = filteredRecipes.filter(item => item.recipe.calories <= range)
+        console.log(filteredRecipes);
+
+        $('.foodImg1').attr('src', filteredRecipes[0].recipe.image)
+        $('.a').html(filteredRecipes[0].recipe.label)
+        $('.foodImg2').attr('src', filteredRecipes[1].recipe.image)
+        $('.b').html(filteredRecipes[1].recipe.label)
+        $('.foodImg3').attr('src', filteredRecipes[2].recipe.image)
+        $('.c').html(filteredRecipes[2].recipe.label)
+        $('.foodImg4').attr('src', filteredRecipes[3].recipe.image)
+        $('.d').html(filteredRecipes[3].recipe.label)
+        $('.foodImg5').attr('src', filteredRecipes[4].recipe.image)
+        $('.e').html(filteredRecipes[4].recipe.label)
+        $('.carousel').carousel();
+    });
 })
 var modal = document.getElementById("myModal");
 
@@ -70,14 +56,14 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-$("#resultsDiv").on("click", ".carousel-item label", function(event) {
+$("#resultsDiv").on("click", ".carousel-item label", function (event) {
     event.preventDefault();
-    modal.style.display = "block"; 
+    modal.style.display = "block";
     $(".img1").css("opacity: 30%;");
 });
 
-$("#myModal").on("click", ".close", function() {
-    modal.style.display = "none";    
+$("#myModal").on("click", ".close", function () {
+    modal.style.display = "none";
 });
 
 
