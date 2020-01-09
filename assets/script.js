3b2c6444b70298bb15de2af63f48183bfc7352cf
Katie88 <kathrynhoth@gmail.com>
2020-01-09 15:10:20 -0700

    removed the extra decimals down to .1

--- assets/script.js
+++ assets/script.js
@@ -81,18 +81,18 @@ $('#carouselLabel').on('click', function (e) {
     $('#nutContainer').append($('<div class="col s3" id="addDiv">'));
     $('#addDiv').append(`<h5>Nutrition Info</h5>`);
     $('#addDiv').append(`<ul class="addList"></ul>`);
-    $(".addList").append($(`<li>Calories: ${filteredRecipes[0].recipe.calories}</li>`));
-    $(".addList").append($(`<li>Protein: ${filteredRecipes[0].recipe.totalNutrients.PROCNT.quantity} g</li>`));
-    $(".addList").append($(`<li>Fat: ${filteredRecipes[0].recipe.totalNutrients.FAT.quantity} g</li>`));
-    $(".addList").append($(`<li>Sat.Fat: ${filteredRecipes[0].recipe.totalNutrients.FASAT.quantity} g</li>`));
-    $(".addList").append($(`<li>Sugar: ${filteredRecipes[0].recipe.totalNutrients.SUGAR.quantity} g</li>`));
-    $(".addList").append($(`<li>Sodium: ${filteredRecipes[0].recipe.totalNutrients.NA.quantity} mg</li>`));
-    $(".addList").append($(`<li>Cholesterol: ${filteredRecipes[0].recipe.totalNutrients.CHOLE.quantity} mg</li>`));
-    $(".addList").append($(`<li>Calcium: ${filteredRecipes[0].recipe.totalNutrients.CA.quantity} mg</li>`));
-    $(".addList").append($(`<li>Magnesium: ${filteredRecipes[0].recipe.totalNutrients.MG.quantity} mg</li>`));
-    $(".addList").append($(`<li>Potassium: ${filteredRecipes[0].recipe.totalNutrients.K.quantity} mg</li>`));
-    $(".addList").append($(`<li>Iron: ${filteredRecipes[0].recipe.totalNutrients.FE.quantity} mg</li>`));
-    $(".addList").append($(`<li>Zinc: ${filteredRecipes[0].recipe.totalNutrients.ZN.quantity} mg</li>`));
+    $(".addList").append($(`<li>Calories: ${filteredRecipes[0].recipe.calories.toFixed(1)}</li>`));
+    $(".addList").append($(`<li>Protein: ${filteredRecipes[0].recipe.totalNutrients.PROCNT.quantity.toFixed(1)} g</li>`));
+    $(".addList").append($(`<li>Fat: ${filteredRecipes[0].recipe.totalNutrients.FAT.quantity.toFixed(1)} g</li>`));
+    $(".addList").append($(`<li>Sat.Fat: ${filteredRecipes[0].recipe.totalNutrients.FASAT.quantity.toFixed(1)} g</li>`));
+    $(".addList").append($(`<li>Sugar: ${filteredRecipes[0].recipe.totalNutrients.SUGAR.quantity.toFixed(1)} g</li>`));
+    $(".addList").append($(`<li>Sodium: ${filteredRecipes[0].recipe.totalNutrients.NA.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Cholesterol: ${filteredRecipes[0].recipe.totalNutrients.CHOLE.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Calcium: ${filteredRecipes[0].recipe.totalNutrients.CA.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Magnesium: ${filteredRecipes[0].recipe.totalNutrients.MG.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Potassium: ${filteredRecipes[0].recipe.totalNutrients.K.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Iron: ${filteredRecipes[0].recipe.totalNutrients.FE.quantity.toFixed(1)} mg</li>`));
+    $(".addList").append($(`<li>Zinc: ${filteredRecipes[0].recipe.totalNutrients.ZN.quantity.toFixed()} mg</li>`));
 
 
     for (var i = 0; i < filteredRecipes[0].recipe.ingredientLines.length; i++) {
